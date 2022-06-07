e930db06ad365765cbd0a94c54a5a2f2c4143daa
Aleix Zamorano <aleix.zamorano@indabasolutions.com>
2022-06-07 09:24:27 +0200

    conflictos merge solucionados 2

--- src/entregaFinal/resources/views/welcome.blade.php
+++ src/entregaFinal/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Pages / Register - NiceAdmin Bootstrap Template</title>
+  <title>Register</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
@@ -18,12 +18,8 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-<<<<<<< HEAD
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-=======
 <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
->>>>>>> Develop
   <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
   <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
   <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
@@ -32,12 +28,8 @@
   <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-<<<<<<< HEAD
-  <link href="assets/css/style.css" rel="stylesheet">
-=======
    <!--  <link href="./assets/css/style.css" rel="stylesheet">-->
    <link href="assets/css/style.css" rel="stylesheet">
->>>>>>> Develop
 
   <!-- =======================================================
   * Template Name: NiceAdmin - v2.2.2
